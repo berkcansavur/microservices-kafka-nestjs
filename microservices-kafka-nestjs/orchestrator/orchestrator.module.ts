893985891e6b26a704924c2ef91e3c5ec29dd85c
berkcansavur <berkcansavur@gmail.com>
2023-10-10 23:04:51 +0300

    update s

--- microservices-kafka-nestjs/orchestrator/orchestrator.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrchestratorService } from './orchestrator.service';
-import { OrchestratorController } from './orchestrator.controller';
-
-@Module({
-  providers: [OrchestratorService],
-  controllers: [OrchestratorController]
-})
-export class OrchestratorModule {}
