f0948636f321cf4946c86a086eb54f234a1c2f6a
berkcansavur <berkcansavur@gmail.com>
2023-10-19 11:15:09 +0300

    swagger implementation to the all app dtos

--- microservices-kafka-nestjs/api-gateaway/src/dtos/api.dtos.ts
+++ microservices-kafka-nestjs/api-gateaway/src/dtos/api.dtos.ts
@@ -50,60 +50,140 @@ export class CreateTransferDTO {
   amount: number;
 }
 export class IncomingTransferRequestDTO {
+  @ApiProperty({
+    description: "Transfers id",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsMongoId()
   _id: string;
 
+  @ApiProperty({
+    description: "Currency type of the transfer",
+    required: true,
+    example: "DOLLAR",
+  })
   @IsString()
   currencyType: string;
 
+  @ApiProperty({
+    description: "Transfer status to be approve",
+    required: false,
+    example: 200,
+  })
   @IsNumber()
   status: string;
 
+  @ApiProperty({
+    description: "User id of the action",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsMongoId()
   userId: string;
 
+  @ApiProperty({
+    description: "Account id of fromAccount",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsMongoId()
   fromAccount: string;
 
+  @ApiProperty({
+    description: "Account id of toAccount",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsMongoId()
   toAccount: string;
 
+  @ApiProperty({
+    description: "Amount of the money that transferred to toAccount",
+    required: true,
+    example: 500,
+  })
   @IsNumber()
   amount: number;
 }
 export class MoneyTransferDTO {
+  @ApiProperty({
+    description: "Currency type of the transfer",
+    required: true,
+    example: "DOLLAR",
+  })
   @IsNotEmpty()
   @IsString()
   currencyType: string;
 
+  @ApiProperty({
+    description: "Amount of the money that transferred to toAccount",
+    required: true,
+    example: 500,
+  })
   @IsNotEmpty()
   @IsNumber()
   amount: number;
 
+  @ApiProperty({
+    description: "Serial no array of the inserted money",
+    required: true,
+    example: [123123123, 879123014, 383819209],
+  })
   @IsNotEmpty()
   serials: number[];
 
+  @ApiProperty({
+    description: "User id of the action",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsNotEmpty()
   @IsMongoId()
   userId: string;
 
+  @ApiProperty({
+    description: "Account id of toAccount",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsNotEmpty()
   @IsMongoId()
   toAccount: string;
 }
 export class CreateBankDTO {
+  @ApiProperty({
+    description: "Created banks name",
+    required: true,
+    example: "ING Bank",
+  })
   @IsNotEmpty()
   @IsString()
   bankName: string;
 
+  @ApiProperty({
+    description: "Bank directors id",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsNotEmpty()
   @IsMongoId()
   bankManager: string;
 
+  @ApiProperty({
+    description: "Bank customer representatives ids array",
+    required: true,
+    examples: ["6530a356aaf92d72d1f0f367", "6530a356aaf92d72d1f0f367"],
+  })
   @IsNotEmpty()
   @IsMongoId()
   customerRepresentatives: object[];
 
+  @ApiProperty({
+    description: "Bank department directors ids array",
+    required: true,
+    examples: ["6530a356aaf92d72d1f0f367", "6530a356aaf92d72d1f0f367"],
+  })
   @IsNotEmpty()
   @IsMongoId()
   departmentDirectors: object[];
@@ -166,23 +246,47 @@ export class CreateCustomerRepresentativeDTO {
 }
 
 export class CreateAccountDTO {
-  @ApiProperty({})
+  @ApiProperty({
+    description: "Accounts owner id",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsNotEmpty()
   @IsMongoId()
   userId: string;
 
+  @ApiProperty({
+    description: "Accounts name",
+    required: true,
+    example: "Berkcan's ING Bank Account",
+  })
   @IsNotEmpty()
   @IsString()
   accountName: string;
 
+  @ApiProperty({
+    description: "Accounts enum type",
+    required: true,
+    example: "BANK_ACCOUNT",
+  })
   @IsNotEmpty()
   @IsString()
   accountType: string;
 
+  @ApiProperty({
+    description: "Bank related with the accounts branch enum key",
+    required: true,
+    example: "KOSUYOLU_BRANCH",
+  })
   @IsNotEmpty()
   @IsString()
   bankBranchCode: string;
 
+  @ApiProperty({
+    description: "Interest percentage of the account",
+    required: true,
+    example: 12,
+  })
   @IsNumber()
   interest?: number;
 }
@@ -224,53 +328,121 @@ export class CreateDepartmentDirectorDTO {
   department: string;
 }
 export class AccountDTO {
+  @ApiProperty({
+    description: "Accounts id",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsMongoId()
   _id: string;
 
+  @ApiProperty({
+    description: "Accounts user id",
+    required: true,
+    example: "6530a356aaf92d72d1f0f367",
+  })
   @IsMongoId()
   userId?: string;
 
+  @ApiProperty({
+    description: "Accounts name",
+    required: true,
+    example: "Berkcan's ING Bank Account",
+  })
   @IsString()
   accountName?: string;
 
+  @ApiProperty({
+    description: "16 digit accounts number",
+    required: true,
+    example: 1292890029912329,
+  })
   @IsNumber()
   accountNumber?: number;
 
+  @ApiProperty({
+    description: "Interest percentage of the account",
+    required: true,
+    example: 12,
+  })
   @IsNumber()
   interest?: number;
 
+  @ApiProperty({
+    description: "Accounts balance array",
+    required: true,
+  })
   balance?: any;
 
+  @ApiProperty({
+    description: "Account status",
+    required: false,
+    example: 200,
+  })
   @IsNumber()
   @IsNotEmpty()
   status?: number;
 
+  @ApiProperty({
+    description: "Accounts actions array",
+    required: true,
+  })
   actionLogs?: any;
 }
 export class CreateCustomerDTO {
+  @ApiProperty({
+    description: "Customers name",
+    required: true,
+    example: "James",
+  })
   @IsNotEmpty()
   @IsString()
   customerName: string;
 
+  @ApiProperty({
+    description: "Customers surname",
+    required: true,
+    example: "Even",
+  })
   @IsNotEmpty()
   @IsString()
   customerSurname: string;
 
+  @ApiProperty({
+    description: "Customers age",
+    required: true,
+    example: 19,
+  })
   @IsNotEmpty()
   @IsNumber()
   @Min(18)
   customerAge: number;
 
+  @ApiProperty({
+    description: "Customers email",
+    required: true,
+    example: "berkcansavurrr@gmail.com",
+  })
   @IsNotEmpty()
   @IsString()
   @IsEmail()
   customerEmail: string;
 
+  @ApiProperty({
+    description: "Customers social security number",
+    required: true,
+    example: 11111111111,
+  })
   @IsNotEmpty()
   @IsNumber()
   @Length(11)
   customerSocialSecurityNumber: number;
 
+  @ApiProperty({
+    description: "Customer password",
+    required: true,
+    example: "examplepassword",
+  })
   @IsNotEmpty()
   @IsString()
   @Length(8, 20)
