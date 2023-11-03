ddd5ae4d94f3b9e80162c0b1ba5564353a3b7784
berkcansavur <berkcansavur@gmail.com>
2023-11-03 06:37:39 +0300

    importing the components needed in app

--- microservices-kafka-nestjs/bank-app/src/app/app.module.ts
+++ microservices-kafka-nestjs/bank-app/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatGridListModule } from "@angular/material/sidenav";
-import { MatMenuModule } from "@angular/material/sidenav";
-import { MatButtonModule } from "@angular/material/sidenav";
-import { MatCardModule } from "@angular/material/sidenav";
-import { MatItemModule } from "@angular/material/sidenav";
-import { MatExpansionModule } from "@angular/material/sidenav";
-import { MatListModule } from "@angular/material/sidenav";
-import { MatToolbarModule } from "@angular/material/sidenav";
-import { MatTableModule } from "@angular/material/sidenav";
-import { MatBadgeModule } from "@angular/material/sidenav";
-import { MatSnackBarModule } from "@angular/material/sidenav";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTableModule } from "@angular/material/table";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -27,7 +27,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     MatMenuModule,
     MatButtonModule,
     MatCardModule,
-    MatItemModule,
+    MatIconModule,
     MatExpansionModule,
     MatListModule,
     MatToolbarModule,
